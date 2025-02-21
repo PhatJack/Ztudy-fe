0699f7c82d88c9bc2eee8924ebee6d7e57b1ef56
PhatJack <tienphatng.693@gmail.com>
2025-02-21 20:36:38 +0700

    Add authentication forms and layout for login and registration

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+import { RegisterForm } from "../components/RegisterForm";
+
+export const metadata: Metadata = {
+  title: "Register",
+};
+
+export default function RegisterPage() {
+  return <RegisterForm />;
+}
