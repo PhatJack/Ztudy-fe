d55dc474f415c86938d610dfecaa9b6dded87584
PhatJack <tienphatng.693@gmail.com>
2025-02-22 11:53:25 +0700

    Add authentication forms and layout for login and registration

--- src/app/(routes)/(auth)/layout.tsx
+++ src/app/(routes)/(auth)/layout.tsx
@@ -8,7 +8,7 @@ export default function AuthLayout({
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <Link href="#" className="flex items-center gap-2 font-semibold">
+          <Link href="/" className="flex items-center gap-2 font-semibold">
             <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <GalleryVerticalEnd className="size-4" />
             </div>
