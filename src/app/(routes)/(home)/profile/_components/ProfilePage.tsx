f4c97447f9afe8dbe0fcaa56bc341f5fa4e94929
PhatJack <tienphat.ng693@gmail.com>
2025-04-07 16:53:18 +0700

    add chart component display

--- src/app/(routes)/(home)/profile/_components/ProfilePage.tsx
+++ src/app/(routes)/(home)/profile/_components/ProfilePage.tsx
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 const ProfileBlock = dynamic(() => import("./ProfileBlock"));
 const ChangePassword = dynamic(() => import("./ChangePassword"));
 const MonthlyLevel = dynamic(() => import("./MonthlyLevel"));
+const ChartBlock = dynamic(() => import("./ChartBlock"));
 
 const ProfilePage = () => {
   return (
@@ -13,7 +14,9 @@ const ProfilePage = () => {
           <ChangePassword />
         </div>
         <div className="xl:col-span-6">
-          <div className="w-full rounded-xl bg-background p-6"></div>
+          <div className="w-full rounded-xl bg-background p-6">
+            <ChartBlock />
+          </div>
         </div>
         <div className="xl:col-span-3 flex flex-col space-y-6">
           <MonthlyLevel />
